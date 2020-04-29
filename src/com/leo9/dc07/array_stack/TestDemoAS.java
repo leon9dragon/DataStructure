73ebd8ff75091c65eb5b1886467c28f21ebbe649
leon9dragon <leon9dragon@gmail.com>
2020-04-29 11:40:11 +0800

    Update TestDemoAS.java

--- src/com/leo9/dc07/array_stack/TestDemoAS.java
+++ src/com/leo9/dc07/array_stack/TestDemoAS.java
@@ -25,7 +25,7 @@ public class TestDemoAS {
         //region 表达式的运算
 
         //定义一个表达式进行测试
-        String expressoion = "3+2*6-2";
+        String expression = "4+6/2-2";
 
         //创建两个栈, 一个数栈一个符号栈
         ArrayStack numStack = new ArrayStack(10);
@@ -36,14 +36,14 @@ public class TestDemoAS {
         int index = 0;
         int num1 = 0;
         int num2 = 0;
-        int oper = ' ';
+        int oper = 0;
         int res = 0;
         char ch = ' ';
 
         //开始循环扫描表达式语句
         while (true){
             //依次获取expression中的每个字符, 每次从字符串中截取一位
-            ch = expressoion.substring(index, index+1).charAt(0);
+            ch = expression.substring(index, index+1).charAt(0);
             //判断ch是什么,然后做相应的处理
             if(operStack.isOper(ch)){
                 //如果是运算符
@@ -58,16 +58,63 @@ public class TestDemoAS {
                         res = numStack.calculate(num1,num2,oper);
                         //把运算结果入数栈
                         numStack.pushData(res);
-                        //把当前操作符入符号栈
-                        operStack.pushData(oper);
+                        //令符号栈最多只有2个操作符, 防止出现减法/除法失误
+                        //把当前操作符入符号栈, 入栈前先判断当前操作符优先级是否小于或者等于栈中的操作符
+                        if(operStack.priority(ch) <= operStack.priority(operStack.getTopData())){
+                            num1 = numStack.popData();
+                            num2 = numStack.popData();
+                            oper = operStack.popData();
+                            res = numStack.calculate(num1,num2,oper);
+                            //把运算结果入数栈
+                            numStack.pushData(res);
+                        }
+                        operStack.pushData(ch);
+                    }
+                    //如果当前的操作符的优先级大于栈中的操作符
+                    else{
+                        operStack.pushData(ch);
                     }
                 }
                 else{
                     //如果为空直接入栈
+                    operStack.pushData(ch);
                 }
+            }
+            else{
+                //如果是数字,则直接入数栈
+                //数字对应ASCII码和实际码数相差48
+                //比如1这个字符对应ASCII码为49, 因此需要减48
+                numStack.pushData(ch - 48);
+
+                //当处理多位数时, 不能立即入栈, 要先判断后一位是否为符号再决定入栈
+
+            }
+            //让index+1,并判断是否扫描到expression最后
+            index++;
+            //当扫描到表达式最后一位的后一位, 即为空的时候, 跳出循环
+            if(index >= expression.length()){
+                break;
             }
         }
 
+        //表达式扫描完毕,顺序的从数栈和符号栈中出栈相应的数和符号, 并运行
+        while(true){
+            //若符号栈为空, 则计算到最后的结果
+            //此时数栈只剩一个数字, 即为最终结果
+            if(operStack.isEmpty()){
+                break;
+            }
+            //开始计算
+            num1 = numStack.popData();
+            num2 = numStack.popData();
+            oper = operStack.popData();
+            res = numStack.calculate(num1,num2,oper);
+            //把运算结果入数栈
+            numStack.pushData(res);
+        }
+
+        //循环结束后数栈只剩结果, 进行输出
+        System.out.println("the result of '"+ expression +"' is: " + numStack.popData());
 
         //endregion
     }
