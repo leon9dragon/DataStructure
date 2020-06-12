56410061ec61df0d69bebba4e5471a1793d5558e
leon9dragon <leon9dragon@gmail.com>
2020-06-12 17:24:45 +0800

    Update TestDemoABT.java

--- src/com/leo9/dc23/array_binary_tree/TestDemoABT.java
+++ src/com/leo9/dc23/array_binary_tree/TestDemoABT.java
@@ -2,13 +2,9 @@ package com.leo9.dc23.array_binary_tree;
 
 public class TestDemoABT {
     public static void main(String[] args) {
-        int[] test_array = {1,2,3,4,5,6,7};
+        int[] test_array = {1,3,6,8,10,14};
         ArrayBinaryTree arr_tree = new ArrayBinaryTree(test_array);
-        System.out.println("======test preOrder======");
-        arr_tree.preOrder(0);
-        System.out.println("======test infixOrder======");
         arr_tree.infixOrder(0);
-        System.out.println("======test postOrder======");
-        arr_tree.postOrder(0);
+
     }
 }
