c100941f80f1e4a77ab0a0ced0381d6538479c92
leon9dragon <leon9dragon@gmail.com>
2020-06-17 17:19:41 +0800

    Create HeapSort.java

---
+++ src/com/leo9/dc25/heap_sort/HeapSort.java
@@ -0,0 +1,22 @@
+package com.leo9.dc25.heap_sort;
+
+public class HeapSort {
+    public static void main(String[] args) {
+        int[] arr = {4,6,8,5,9};
+    }
+
+    //编写一个堆排序方法
+    public static void heapSort(int[] arr){
+        System.out.println("堆排序");
+    }
+
+    //将数组(二叉树)调整成一个大顶堆
+    /**
+     * @param arr 待调整数组
+     * @param i 非叶子结点在数组中索引
+     * @param length 需要调整的元素个数
+     * */
+    public static void  adjustHeap(int[] arr, int i, int length){
+
+    }
+}
