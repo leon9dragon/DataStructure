adf2d7072b730325e9c8288096dd509d45107403
leon9dragon <leon9dragon@gmail.com>
2020-07-08 16:58:59 +0800

    Update

---
+++ src/com/leo9/dc28/binary_sort_tree/BinarySortTree.java
@@ -0,0 +1,4 @@
+package com.leo9.dc28.binary_sort_tree;
+
+public class BinarySortTree {
+}
---
+++ src/com/leo9/dc28/binary_sort_tree/TreeNode.java
@@ -0,0 +1,17 @@
+package com.leo9.dc28.binary_sort_tree;
+
+public class TreeNode {
+    public int value;
+    public TreeNode left_node;
+    public TreeNode right_node;
+
+    public TreeNode(int value) {
+        this.value = value;
+    }
+
+    //添加结点的方法
+    //递归形式添加结点, 注意需要满足二叉排序树的要求
+    public void addNode(TreeNode new_node){
+        
+    }
+}
