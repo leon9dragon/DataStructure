4512c9d8f4efd641304457fceb29b7602a51013b
leon9dragon <49860984+leon9dragon@users.noreply.github.com>
2020-08-25 19:59:30 +0800

    Update RemoveElement.java

--- src/com/leetcode/array_problem/p0027/RemoveElement.java
+++ src/com/leetcode/array_problem/p0027/RemoveElement.java
@@ -4,12 +4,12 @@ import java.util.Arrays;
 
 public class RemoveElement {
     public static void main(String[] args) {
-        int[] nums = {3, 2, 2, 3};//{0, 1, 2, 2, 3, 0, 4, 2}
+        int[] nums = {0, 1, 2, 2, 3, 0, 4, 2, 2};//{0, 1, 2, 2, 3, 0, 4, 2}{3, 2, 2, 3}
         System.out.println("========================");
         System.out.println("length of origin array = " + nums.length);
         System.out.println("origin array = " + Arrays.toString(nums));
 
-        int target = 3;
+        int target = 2;
         int res = removeElement(nums, target);
         System.out.println();
         System.out.println("the target = " + target);
@@ -28,31 +28,16 @@ public class RemoveElement {
 
     public static int removeElement(int[] nums, int val) {
         if (nums.length == 0) return 0;
-        //define two pointer
-        //one is pointed to the head of array, from head to tail
-        //another is pointed to the tail of array, from tail to head
-        int h_point = 0;
-        int t_point = nums.length - 1;
-
-        while (h_point < t_point) {
-            if (nums[h_point] != val) {
-                h_point++;
-            }
-
-            if (nums[t_point] == val) {
-                t_point--;
-            }
-
-            if (nums[h_point] == val && nums[t_point] != val) {
-                nums[h_point] += nums[t_point];
-                nums[t_point] = nums[h_point] - nums[t_point];
-                nums[h_point] -= nums[t_point];
-                h_point++;
-                t_point--;
+        int pointer = 0;
+        int length = nums.length;
+        while (pointer < length) {
+            if (nums[pointer] == val) {
+                nums[pointer] = nums[length - 1];
+                length--;
+            } else {
+                pointer++;
             }
         }
-
-        //the length of array equals to h_point's value + 1
-        return h_point;
+        return length;
     }
 }
