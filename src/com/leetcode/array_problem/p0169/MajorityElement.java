b5eb82abe662ac881e998e7867f021f713bbb4e1
leon9dragon <leon9dragon@gmail.com>
2020-08-28 17:30:56 +0800

    Update MajorityElement.java

--- src/com/leetcode/array_problem/p0169/MajorityElement.java
+++ src/com/leetcode/array_problem/p0169/MajorityElement.java
@@ -1,11 +1,21 @@
 package com.leetcode.array_problem.p0169;
 
+import java.util.HashMap;
+
 public class MajorityElement {
     public static void main(String[] args) {
 
     }
 
     public int majorityElement(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i])) {
+                map.put(nums[i], map.get(nums[i]) + 1);
+            } else {
+                map.put(nums[i], 1);
+            }
+        }
         
     }
 }
