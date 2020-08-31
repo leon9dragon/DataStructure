f55389e733b147d3f0d7295b1dbb610bbb95dd47
leon9dragon <49860984+leon9dragon@users.noreply.github.com>
2020-09-01 00:47:23 +0800

    Update RotateArray.java

--- src/com/leetcode/array_problem/p0189/RotateArray.java
+++ src/com/leetcode/array_problem/p0189/RotateArray.java
@@ -4,8 +4,8 @@ import java.util.Arrays;
 
 public class RotateArray {
     public static void main(String[] args) {
-        int[] nums = {1, 2, 3, 4, 5, 6};
-        int k = 2;
+        int[] nums = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int k = 3;
         System.out.println("======================");
         System.out.println("origin array = " + Arrays.toString(nums));
         rotate(nums, k);
