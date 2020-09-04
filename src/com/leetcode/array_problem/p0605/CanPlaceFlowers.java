1a6917fd264904f4d0de41cefca696bad01db598
leon9dragon <leon9dragon@gmail.com>
2020-09-04 16:24:25 +0800

    Create CanPlaceFlowers.java

---
+++ src/com/leetcode/array_problem/p0605/CanPlaceFlowers.java
@@ -0,0 +1,67 @@
+package com.leetcode.array_problem.p0605;
+
+import javafx.scene.SceneAntialiasing;
+
+import java.util.Arrays;
+
+public class CanPlaceFlowers {
+    public static void main(String[] args) {
+        int[] flowerbed = {0, 0, 0, 0, 0};
+        int n = 4;
+        System.out.println("============================");
+        System.out.println("the array = " + Arrays.toString(flowerbed));
+        System.out.printf("the new flowers nuber is [%d]\n", n);
+        boolean res = canPlaceFlowers(flowerbed, n);
+
+        if (res) {
+            System.out.println("the plots are enough!");
+        } else {
+            System.out.println("the plots are not enough!");
+        }
+    }
+
+    public static boolean canPlaceFlowers(int[] flowerbed, int n) {
+        int count = 0;
+        int place = 0;
+        int sign;
+
+        for (sign = 0; sign < flowerbed.length; sign++) {
+            if (flowerbed[sign] == 0) {
+                count++;
+            } else {
+                break;
+            }
+        }
+
+        if (count != 0) {
+            if (count == flowerbed.length) {
+                count = count % 2 == 0 ? count - 1 : count;
+                place = count / 2 + 1;
+            } else {
+                place += count / 2;
+            }
+            count = 0;
+        }
+
+
+        for (int i = sign; i < flowerbed.length; i++) {
+            if (flowerbed[i] == 0) {
+                count++;
+            } else {
+                if (count != 0) {
+                    count = count % 2 == 0 ? count - 1 : count;
+                    place += count / 2;
+                    count = 0;
+                }
+            }
+        }
+
+        if (count != 0) {
+            place += count / 2;
+        }
+
+        System.out.printf("the number of places can be placed flower is [%d]\n", place);
+
+        return place >= n;
+    }
+}
