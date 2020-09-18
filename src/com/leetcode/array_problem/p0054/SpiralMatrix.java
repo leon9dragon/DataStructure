11601e09cfb0a1afb41759ff0fb8121863fab0c6
leon9dragon <leon9dragon@gmail.com>
2020-09-18 17:20:00 +0800

    Create SpiralMatrix.java

---
+++ src/com/leetcode/array_problem/p0054/SpiralMatrix.java
@@ -0,0 +1,22 @@
+package com.leetcode.array_problem.p0054;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SpiralMatrix {
+    public static void main(String[] args) {
+
+    }
+
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> resList = new ArrayList<>();
+        int ele_num = matrix.length * matrix[0].length;
+        int count = 0;
+        int h_ptr
+        while (count <= ele_num) {
+
+            count++;
+        }
+        return resList;
+    }
+}
