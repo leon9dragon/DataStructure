0197da25219b026cdd518802b19383d43cdea082
leon9dragon <leon9dragon@gmail.com>
2020-11-05 17:33:30 +0800

    Update InsertInterval.java

--- src/com/leetcode/array_problem/p0057/InsertInterval.java
+++ src/com/leetcode/array_problem/p0057/InsertInterval.java
@@ -19,15 +19,45 @@ public class InsertInterval {
     }
 
     public static int[][] insert(int[][] intervals, int[] newInterval) {
-        int new_sp = newInterval[0];
-        int new_ep = newInterval[1];
+        int[][] res;
+        int n_sp;
+        int n_ep = newInterval[1];
 
-        
+        int count = 0;
+        int s_ptr = 0, e_ptr = 0;
+
 
         for (int[] row : intervals) {
+            s_ptr = count;
 
+            if (!(newInterval[0] > row[1])) {
+                if (newInterval[0] <= row[0]) {
+                    n_sp = newInterval[0]
+                } else {
+                    n_sp = row[0];
+                }
+                break;
+            }
+
+            count++;
         }
 
+        if (count >= intervals.length) {
+            res = new int[intervals.length + 1][];
+            res[res.length - 1] = newInterval;
+            for (int i = 0; i < res.length - 1; i++) {
+                res[i] = intervals[i];
+            }
+            return res;
+        }
+
+        for (int i = s_ptr; i < intervals.length; i++) {
+            e_ptr = count;
+
+            if (!(newInterval[1]>intervals[i][1])){
+                
+            }
+        }
 
     }
 }
