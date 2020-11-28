f5391551a9080dd5506a094cb2371f44265e9eb4
leon9dragon <leon9dragon@gmail.com>
2020-11-28 10:58:07 +0800

    Update

---
+++ src/com/leetcode/linkedlist_problem/p0021/MergeTwoSortedLists.java
@@ -0,0 +1,41 @@
+package com.leetcode.linkedlist_problem.p0021;
+
+
+//Definition for singly-linked list.
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class MergeTwoSortedLists {
+    public static void main(String[] args) {
+
+    }
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode ptr1, ptr2;
+
+        ptr1 = l1;
+        ptr2 = l2;
+        while (ptr1 != null && ptr2 != null) {
+            
+        }
+
+
+        return null;
+    }
+}
+
+
